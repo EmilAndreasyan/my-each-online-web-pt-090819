d178d4d461f6e4f2d9176bc8188b231dcde11692
Emil Andreasyan <emil.andreasyan@yahoo.com>
2019-09-18 02:40:39 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,13 +1,3 @@
-# words = ['hi', 'hello', 'bye', 'goodbye']
-# def my_each(words)
-# i = 0
-# while i < words.length
-# yield words[i]
-# i += 1
-# end
-# end
-# my_each(words)
-
 def my_each(collection)
   i = 0
   while i < collection.length
